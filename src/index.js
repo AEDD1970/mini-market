1000e18f949bb3c0a2c03ad16fac1e2282fa7a7b
Alexis Duque <alexis.duque@bewe.io>
2022-10-27 00:08:10 -0500

    add structure for mini-market

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store, persistor, sagaMiddleware } from './redux/store';
+import rootSaga from './redux/sagas'
+import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Routes as Routing, Route } from "react-router-dom";
+
+export default function Routes() {
+  return (
+    <Routing>
+
+    </Routing>
+  )
+}
